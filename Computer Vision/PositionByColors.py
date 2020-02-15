4081bfd603e74f554e238e41e6878f7c9ae7de85
Juan Pablo Rivera Ruiz <jprr.2000@hotmail.com>
2020-02-15 17:08:51 -0600

    Successful tracking

--- Computer Vision/PositionByColors.py
+++ Computer Vision/PositionByColors.py
@@ -8,6 +8,8 @@ def to255(num, val):
     x = (num*255)//val
     return x
 
+#--------------------------------------------------------------------------------------------------------------
+
 boundaries = [
         ([to255(20,360), to255(40,100), to255(60,100)], [to255(65,360), to255(100,100), to255(100,100)]),     #AMARILLO
         ([to255(100,360), to255(35,100), to255(40,100)], [to255(160,360), to255(100,100), to255(100,100)]),   #AZUL
@@ -18,17 +20,18 @@ boundaries = [
         ([to255(200,360), to255(20,100), to255(75,100)], [to255(360,360), to255(100,100), to255(100,100)]),   #ROSA
         ]
 
+#-------------------------------------------------------------------------------------------------------------------
+
 def display(img, cmap = None):
     fig = plt.figure(figsize = (12,10))
     ax = fig.add_subplot(111)
     ax.imshow(img, cmap)
+    
+#-----------------------------------------------------------------------------------------------------------------
 
 def find_center(img, color):
-    
     global boundaries
-    
     centers = []
-
     kernel = np.ones(shape = (4,4), dtype = np.uint8)
     rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -84,6 +87,8 @@ def find_center(img, color):
         return centers
     return None
 
+#-------------------------------------------------------------------------------------------------------------------
+
 def min_distance(pt1, iterable):
     x1 = pt1[0][0]
     y1 = pt1[0][1]
@@ -96,6 +101,8 @@ def min_distance(pt1, iterable):
             min_center = i 
     return min_center
 
+#-------------------------------------------------------------------------------------------------------------------
+
 def draw_line(img, c1, c2):
     min_c = min_distance(c1, c2)
     cv2.line(img, c1[0], c2[min_c], color = 255, thickness = 2)
@@ -103,21 +110,88 @@ def draw_line(img, c1, c2):
     cv2.circle(img, c2[min_c], radius = 3, color = (0,255,0), thickness = -1)
     return img
 
-img = cv2.imread('C:/Users/jprr2/Videos/Logitech/LogiCapture/cancha.jpg')
-cancha = img[280:680, 360:930, :]
+#-------------------------------------------------------------------------------------------------------------------
 
-c1 = find_center(cancha, "purple")
-c2 = find_center(cancha, "yellow")
-draw_line(cancha, c1, c2)
+def find_robot_center(img, color1, color2):
+    centers1 = find_center(img, color1)
+    centers2 = find_center(img, color2)
+    
+    if centers1 is not None and centers2 is not None:
+        c1 = centers1[0]
+        c2 = centers2[min_distance(centers1, centers2)]
+        robot_center = (c1[0]+c2[0]) // 2, (c1[1]+c2[1]) // 2
+        return [c1, c2, robot_center]
+    return None
 
-rgb_img = cv2.cvtColor(cancha, cv2.COLOR_BGR2RGB)
+#---------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------
+
+lk_params = dict(winSize = (200,200), maxLevel = 2, criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+cap = cv2.VideoCapture(1)
+
+prevPts = None
+
+while prevPts is None:
+
+    ret, frame = cap.read()
+
+    prev_cancha = frame[280:680, 360:930, :]
+
+    prev_gray = cv2.cvtColor(prev_cancha, cv2.COLOR_BGR2GRAY)
+
+    prevPts = find_robot_center(prev_cancha, "orange", "blue")
+
+
+mask = np.zeros_like(prev_cancha) 
 
 while True:
-    cv2.imshow("Image",rgb_img)
+
+    ret, frame =  cap.read()
+
+    cancha = frame[280:680, 360:930, :]
+
+    frame_gray = cv2.cvtColor(cancha, cv2.COLOR_BGR2GRAY)
+
+    nextPts, status, err = cv2.calcOpticalFlowPyrLK(prev_gray, frame_gray, prevPts, None, **lk_params)  #STATUS RETURNS 1 IF ANY FLOW HAS BEEN FOUND
+
+    good_new = nextPts[status == 1]
+    good_prev = prevPts[status == 1]
+
+    for i, (new, prev) in enumerate(zip(good_new, good_prev)):
+
+        x_new, y_new = new.ravel()            #TURNS MULTIDIMENSIONAL ARRAYS INTO A SINGLE DIMENTION ARRAY
+        x_prev, y_prev = prev.ravel()
+
+        mask = cv2.line(mask, (x_new,y_new), (x_prev,y_prev), color = (0,255,0), thickness = 3)
+
+        frame = cv2.circle(frame, (x_new,y_new), radius = 8, color = (0,0,255), thickness = -1)
+
+    img = cv2.add(frame, mask)
+    cv2.imshow("Tracking", img)
 
     k = cv2.waitKey(1)
 
     if k == 27:
         break
 
+    prev_gray = frame_gray.copy()
+    prevPts = good_new.reshape(-1,1,2)
+
+
+cap.release()
 cv2.destroyAllWindows()
+
+"""img = cv2.imread('C:/Users/jprr2/Videos/Logitech/LogiCapture/cancha.jpg')
+            
+cancha = img[280:680, 360:930, :]
+                 
+c1, c2, robot_center = find_robot_center(cancha, "orange", "blue")
+                 
+cv2.line(cancha, c1, c2, color = 255, thickness = 2)
+cv2.circle(cancha, robot_center, radius = 3, color = (0,255,0), thickness = -1)
+                 
+rgb_img = cv2.cvtColor(cancha, cv2.COLOR_BGR2RGB)
+                 
+display(rgb_img)"""
